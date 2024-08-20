de135eb994196a4be162fe95dd789133820772df
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-20 15:33:50 +0300

    Add project files.

---
+++ PDFReports/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PDFReports.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
