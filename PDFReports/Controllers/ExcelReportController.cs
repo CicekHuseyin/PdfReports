d5eedf176a488c77eed75de83034c3e12a492709
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-20 16:19:06 +0300

    Yeni dosaylar oluşturarak verileri içine yazdırıp okuduk.

---
+++ PDFReports/Controllers/ExcelReportController.cs
@@ -0,0 +1,37 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDFReports.Controllers
+{
+    public class ExcelReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CustomerExcelReport()
+        {
+            var workBook = new XLWorkbook();
+            var workSheet = workBook.Worksheets.Add("Müşteri Listesi");
+            workSheet.Cell(1, 1).Value = "Müşteri ID";
+            workSheet.Cell(1, 2).Value = "Müşteri Adı";
+            workSheet.Cell(1, 3).Value = "Müşteri Soyadı";
+            workSheet.Cell(1, 4).Value = "Müşteri Şehri";
+
+            workSheet.Cell(2, 1).Value = "1";
+            workSheet.Cell(2, 2).Value = "Hüseyin";
+            workSheet.Cell(2, 3).Value = "Çiçek";
+            workSheet.Cell(2, 4).Value = "Van";
+
+            workSheet.Cell(3, 1).Value = "2";
+            workSheet.Cell(3, 2).Value = "Hasan";
+            workSheet.Cell(3, 3).Value = "Çiçek";
+            workSheet.Cell(3, 4).Value = "Bursa";
+
+            var stream = new MemoryStream();
+            workBook.SaveAs(stream);
+            var content = stream.ToArray();
+            return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetnl.sheet", "customerlist.xlsx");
+        }
+    }
+}
---
+++ PDFReports/Controllers/PdfReportController.cs
@@ -0,0 +1,53 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDFReports.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StaticPdfReport()
+        {
+            //Pdf Rapor oluşturuldu. Ve indirildi.
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/" + "file1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+            Paragraph paragraph = new Paragraph("Raporumuz Hazırlandı.");
+            document.Add(paragraph);
+            document.Close();
+            return File("/pdfreports/file1.pdf", "application/pdf", "file1.pdf");
+        }
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/" + "file2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+            var document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+            document.OpenDocument();
+            //3 sütün sayısı
+            PdfPTable pdfPTable = new PdfPTable(3);
+            pdfPTable.AddCell("Müşteri Adı");
+            pdfPTable.AddCell("Müşteri Soyadı");
+            pdfPTable.AddCell("Müşteri Şehri");
+
+            pdfPTable.AddCell("Hüseyin");
+            pdfPTable.AddCell("Çiçek");
+            pdfPTable.AddCell("Van");
+
+            pdfPTable.AddCell("Hasa");
+            pdfPTable.AddCell("Çiçek");
+            pdfPTable.AddCell("İstanbul");
+
+            document.Add(pdfPTable);
+            document.Close();
+            return File("/pdfreports/file2.pdf", "application/pdf", "file2.pdf");
+
+        }
+    }
+}
